6a465e22e688db31ed21ba704e1b9d7ac6006ea0
Cody Harrington <thecodeman66@hotmail.com>
2014-11-28 03:52:53 +1300

    Updated bf to include a call stack. Need to fix the eip in parse to use an int offset instead of a pointer

--- bf.c
+++ bf.c
@@ -1,7 +1,4 @@
-/**
- * @file bf.c
- * @author Cody Harrington
- * @details Simple brainf**k parser written in ANSI C. Compiled with
+ /**
  * gcc -g -ansi -Wall -Wextra -Werror -pedantic -pedantic-errors -o bf bf.c
  * Run as:
  * ./bf somestring
@@ -10,83 +7,132 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define PROGRAM_SIZE_BYTES 30000
+#define CALL_STACK_SIZE 25000
+#define MAX_INPUT_LEN 2500
+#define EXIT_LOOP -1
+#define ENTER_LOOP_SYM 'S'
 
-int parse(unsigned char *buffer, const char *src)
+#define STACK_FULL -1
+#define STACK_EMPTY -2
+#define CHECK_STACK_EMPTY(stack) \
+	(stack->ebp == stack->esp)
+
+struct STACK {
+	int *_stack;
+	int *ebp;
+	int *esp;
+	size_t size;
+};
+
+struct STACK *init_stack(struct STACK *stack, size_t size) {
+	stack = malloc(sizeof(struct STACK));
+	stack->_stack = calloc(size, sizeof(int));
+	stack->size = size;
+	stack->ebp = stack->esp = stack->_stack;
+	return stack;
+}
+
+void destroy_stack(struct STACK *stack) {
+	free(stack->_stack);
+	free(stack);
+}
+
+int pop_stack(struct STACK *stack) {
+	if (stack->esp == stack->ebp)
+		return STACK_EMPTY;
+	int item = *stack->esp;
+	*stack->esp = '\0';
+	--(stack->esp);
+	return item;
+}
+
+int push_stack(struct STACK *stack, int item) {
+	if ((size_t)(stack->esp - stack->ebp) >= stack->size)
+		return STACK_FULL;
+	++(stack->esp);
+	*stack->esp = item;
+	return EXIT_SUCCESS;
+}
+
+int parse(struct STACK *_call_stack, unsigned char *buffer, char *src)
 {
-    unsigned char *ebp = buffer;
-    int srclen = strlen(src);
-    int eip = 0;
-    int depth = 0;
+	unsigned char *offset = buffer;
+	char *eip = src;
 
-    while (src[eip]) {
-        /*printf("depth %d, eip %d, ebp: %d\n", depth, eip, *ebp);*/
-        switch (src[eip]) {
-            case '>':
-                ++ebp;
-                break;
-            case '<':
-                --ebp;
-                break;
-            case '+':
-                ++*ebp;
-                break;
-            case '-':
-                --*ebp;
-                break;
-            case '.':
-                putchar(*ebp);
-                break;
-            case ',':
-                *ebp = getchar();
-                break;
-            case '[':
-                if (!*ebp) {
-                    depth = 1;
-                    while ((eip < srclen) && (depth > 0)) {
-                        ++eip;
-                        if (src[eip] == '[') { depth++; }
-                        else if (src[eip] == ']') { depth--; }
-                    }
-                }
-                break;
-            case ']':
-                if (*ebp) {
-                    depth = -1;
-                    while (eip && (depth < 0)) {
-                        --eip;
-                        if (src[eip] == ']') { depth--; }
-                        else if (src[eip] == '[') { depth++; }
-                    }
-                }
-                break;
-        }
-        
-        if (depth < 0) {
-            printf("Error: Unclosed ]");
-            return EXIT_FAILURE;
-        }
-        else if (depth > 0) {
-            printf("Error: Unclosed [");
-            return EXIT_FAILURE;
-        }
-        else { ++eip; }
-    }
-    return EXIT_SUCCESS;
+	while (*eip) {
+		switch (*eip) {
+		    case '>':
+			++offset;
+			break;
+		    case '<':
+			--offset;
+			break;
+		    case '+':
+			++*offset;
+			break;
+		    case '-':
+			--*offset;
+			break;
+		    case '.':
+			putchar(*offset);
+			break;
+		    case ',':
+			*offset = getchar();
+			break;
+		    case '[':
+			++eip;
+			if (push_stack(_call_stack, ENTER_LOOP_SYM) == STACK_FULL) {
+				printf("Max recursion depth of %d exceeded.\n", CALL_STACK_SIZE);	
+				goto fail_cleanup;
+			}
+			parse(_call_stack, offset, eip);
+			break;
+		    case ']':
+			++eip;
+			if (pop_stack(_call_stack) == STACK_EMPTY) {
+				printf("Program terminated early. Unmatched exit loop brace ( ] )\n");
+				goto fail_cleanup;
+			}
+			return EXIT_LOOP;
+			break;
+		}
+		++eip;
+	}
+
+	if (!CHECK_STACK_EMPTY(_call_stack)) {
+		printf("Program terminated early. Unmatched enter loop brace ( [ )\n");
+		goto fail_cleanup;
+	}
+
+	return EXIT_SUCCESS;
+
+fail_cleanup:
+	destroy_stack(_call_stack);
+	exit(EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[])
 {
-    unsigned char memory[PROGRAM_SIZE_BYTES] = {'\0'};
-    char *bf_string = '\0';
+	unsigned char memory[PROGRAM_SIZE_BYTES] = {'\0'};
+	char bf_string[MAX_INPUT_LEN + 1] = {'\0'};
+	struct STACK *call_stack = NULL;
+	unsigned int status = 0;
 
-    if ((argc == 2) && argv[1]) {
-        bf_string = argv[1];
-    } 
-    else {
-        bf_string = "";
-    }
+	call_stack = init_stack(call_stack, CALL_STACK_SIZE);
 
-    return parse(memory, bf_string);
+	if ((argc == 2) && argv[1]) {
+		strncpy(bf_string, argv[1], MAX_INPUT_LEN);
+	} else if (argc != 2) {
+		printf("You must provide one argument\n");
+		return EXIT_FAILURE;
+	}
+	status = parse(call_stack, memory, bf_string);
+	write(STDOUT_FILENO, memory, PROGRAM_SIZE_BYTES);
+
+	destroy_stack(call_stack);
+
+	return status;
 }
